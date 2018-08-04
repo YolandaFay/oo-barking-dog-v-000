dc16b360fad0777941b4e8d8eb54418b9d82907b
Yolanda Fay <yolandafay@gmail.com>
2018-08-04 02:24:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Dog 
+
+  def initialize(dogs_name, dogs_breed="Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+ 
+  def name
+    @name 
+  end
+  
+  def breed=(dogs_breed)
+    @breed = dogs_breed 
+  end
+ 
+  def breed 
+    @breed 
+  end
+  
+end
